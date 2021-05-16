3993558c5f38e924af575844e8e0fc4edf786531
SoTypicalNoob <so.typical.noob@gmail.com>
2021-05-16 15:20:34 +0200

    plotting total expenses vs total budget

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,5 @@
 from . import Expense
+import matplotlib.pyplot as plt
 
 
 class BudgetList(object):
@@ -23,6 +24,17 @@ class BudgetList(object):
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
 
 def main():
     myBudgetList = BudgetList(1200)
@@ -32,6 +44,15 @@ def main():
         myBudgetList.append(expense.amount)
         pass
     print("The count of all expenses: ", str(len(myBudgetList)))
+    for entry in myBudgetList:
+        print(entry)
+        pass
+    fig, ax = plt.subplots()
+    labels = ['Expenses','Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green','red', 'blue'])
+    ax.set_title('Your total lexpenses vs. total budget')
+    plt.show()
 
 
 if __name__ == "__main__":
